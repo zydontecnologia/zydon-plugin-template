cea611e09a03c9ab5410b35a2bcb071a24e17c20
Marcos Vinicius <marcosbts71@gmail.com>
2025-10-07 12:03:09 -0300

    Ajustes e melhorias

--- frontend/src/utils/plugin.tsx
+++ frontend/src/utils/plugin.tsx
@@ -1,65 +1,12 @@
-import ReactDOM from 'react-dom/client';
+import { buildPlugin as buildPluginUtil } from '@zydon/plugin/utils/plugin';
 
 import packageJson from '../../package.json';
-
-function createApp<P>(Component: React.ComponentType<P>) {
-  return class ReactElement extends HTMLElement {
-    private mountPoint: HTMLDivElement;
-    private root: ReactDOM.Root | null = null;
-    private _props: Record<string, unknown> = {};
-
-    constructor() {
-      super();
-      this.mountPoint = document.createElement('div');
-      this.mountPoint.style.width = '100%';
-    }
-
-    set props(v: Record<string, unknown>) {
-      this._props = v || {};
-      this.render();
-    }
-
-    get props() {
-      return this._props;
-    }
-
-    connectedCallback() {
-      this.appendChild(this.mountPoint);
-      this.upgradeProperty('props');
-
-      if (!this.root) {
-        this.root = ReactDOM.createRoot(this.mountPoint);
-      }
-      this.render();
-    }
-
-    disconnectedCallback() {
-      this.root?.unmount();
-    }
-
-    private upgradeProperty(prop: string) {
-      if (Object.prototype.hasOwnProperty.call(this, prop)) {
-        const value = (this as Record<string, unknown>)[prop];
-        delete (this as Record<string, unknown>)[prop];
-        (this as Record<string, unknown>)[prop] = value;
-      }
-    }
-
-    render() {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      this.root?.render(<Component {...(this.props as any)} />);
-    }
-  };
-}
 
 export function buildPlugin<P>(
   Component: React.ComponentType<P>,
   name: string,
 ) {
-  const tagName = `plugin-${name}-${packageJson.name}`;
+  const appName = packageJson.name;
 
-  if (!customElements.get(tagName)) {
-    const Element = createApp(Component);
-    customElements.define(tagName, Element);
-  }
+  return buildPluginUtil(Component, appName, name);
 }
